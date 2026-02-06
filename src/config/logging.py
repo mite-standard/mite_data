9462bac48bbf0b32f05ceff815f5b2ac1d0cef02
Mitja M. Zdouc <mmzdouc@gmail.com>
2026-02-06 16:32:47 +0100

    Added logger

---
+++ src/config/logging.py
@@ -0,0 +1,9 @@
+import logging
+import sys
+
+def setup_logger():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
